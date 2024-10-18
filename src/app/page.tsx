b34bfd71625c68a964393cfac14f16ddd5100d11
Gabbana <chris.lund02@gmail.com>
2024-10-18 17:43:24 +0200

    added img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function HomePage() {
-  return <></>;
+  return (
+    <>
+      <section className="Introduction">
+        <img src="" alt="" />
+      </section>
+    </>
+  );
 }
