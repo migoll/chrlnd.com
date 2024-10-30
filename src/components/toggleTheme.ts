79be3ab5f0441333ffb7b93ecb868df4bf2bb162
Gabbana <chris.lund02@gmail.com>
2024-10-30 18:52:00 +0100

    added comment

--- src/components/toggleTheme.ts
+++ src/components/toggleTheme.ts
@@ -1,3 +1,4 @@
+// handleToggleTheme dark mode function inspireret af https://tailwindcss.com/docs/dark-mode#supporting-system-preference-and-manual-selection
 export function toggleTheme() {
   const isDark = document.documentElement.classList.contains("dark");
   if (isDark) {
