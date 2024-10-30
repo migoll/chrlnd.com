80c53635d6086d3ee07968f7f9d2741639703048
Gabbana <chris.lund02@gmail.com>
2024-10-30 16:55:48 +0100

    dark mode implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function HomePage() {
         </div>
         <div className="flex gap-6 items-center">
           <a href="mailto:chris.lund02@gmail.com">
-            <button className="text-2xl font-medium border-3 border-black rounded-full py-3 px-5 hover:bg-neutral-900 hover:text-neutral-100">
+            <button className="text-2xl font-medium border-3 border-black rounded-full py-3 px-5 hover:bg-neutral-900 hover:text-neutral-100 dark:border-white">
               Reach out
             </button>
           </a>
@@ -146,7 +146,7 @@ export default function HomePage() {
           <h2 className="text-xl font-bold">Web design</h2>
           <h3 className="text-neutral-500">Figma</h3>
         </div>
-        <p className="text-lg text-center text-neutral-600 font-normal">
+        <p className="text-lg text-center text-neutral-600 dark:text-neutral-300 font-normal">
           I began designing in Figma because of my interests in the Web3 space,
           and interest in emoji/emote design on online streaming platforms.
         </p>
@@ -156,7 +156,7 @@ export default function HomePage() {
           <h2 className="text-xl font-bold">Front-end development</h2>
           <h3 className="text-neutral-500">HTML, CSS, JS</h3>
         </div>
-        <p className="text-lg text-center text-neutral-600 font-normal">
+        <p className="text-lg text-center text-neutral-600 dark:text-neutral-300 font-normal">
           I started studying Multimedia design, where I started with learning
           HTML, CSS, and then JS. Ended up using the framework Vue.
         </p>
@@ -166,7 +166,7 @@ export default function HomePage() {
           <h2 className="text-xl font-bold">Frameworks</h2>
           <h3 className="text-neutral-500">Vue, React, TSX, Tailwind</h3>
         </div>
-        <p className="text-lg text-center text-neutral-600 font-normal">
+        <p className="text-lg text-center text-neutral-600 dark:text-neutral-300 font-normal">
           I initially focused on Vue for school work, but I became interested in
           learning other tools like React and TypeScript. Tailwind CSS quickly
           became my go-to for efficiently styling modern interfaces.
